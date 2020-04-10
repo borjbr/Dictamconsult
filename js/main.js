d866b0be8fc054f67112d80fadb4c764459402e4
borjacarretero <borja.carretero@babel.es>
2020-04-10 12:36:28 +0200

    add changes tabnav

--- js/main.js
+++ js/main.js
@@ -245,3 +245,23 @@ $(function() {
   }
 })();
 
+//cambiar logotipo scroll
+
+$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  
+      if (scroll >= 500) {
+        if(!logo.hasClass("sml-logo")) {
+          logo.hide();
+          logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
+          $(".c-header").css("height", "60px").fadeIn( "slow");
+        }
+      } else {
+        if(!logo.hasClass("lrg-logo")) {
+          logo.hide();
+          logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
+          $(".c-header").css("height", "120px").fadeIn( "slow");
+        }
+      }
+  });
+});
